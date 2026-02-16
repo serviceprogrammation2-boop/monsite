d71bd6fdb27c8b33f36798e6cbd20819f7822de9
hedi <bmedh@hotmail.com>
2026-02-16 14:17:22 +0100

    Optimisation filtres employe et equipement

--- blog/forms.py
+++ blog/forms.py
@@ -2,28 +2,35 @@
 from django import forms
 from django.forms import modelformset_factory
 from .models import Navette, Employe, Equipement
+from django.db.models import Q
 
 class NavetteForm(forms.ModelForm):
     achauffeur = forms.ModelChoiceField(
-        queryset=Employe.objects.all(),
+        queryset=Employe.objects.filter(mat_emp__gte=14650),
         required=False,
         widget=forms.Select(attrs={'class': 'select2'})
     )
 
     rchauffeur = forms.ModelChoiceField(
-        queryset=Employe.objects.all(),
+        queryset=Employe.objects.filter(mat_emp__gte=14650),
         required=False,
         widget=forms.Select(attrs={'class': 'select2'})
     )
 
     aveh = forms.ModelChoiceField(
-        queryset=Equipement.objects.all(),
+        queryset=Equipement.objects.filter(
+        Q(cod_equ__gte='00250', cod_equ__lte='00530') |
+        Q(cod_equ__gte='02001', cod_equ__lte='04090')
+    ),
         required=False,
         widget=forms.Select(attrs={'class': 'select2'})
     )
 
     rveh = forms.ModelChoiceField(
-        queryset=Equipement.objects.all(),
+        queryset=Equipement.objects.filter(
+        Q(cod_equ__gte='00250', cod_equ__lte='00530') |
+        Q(cod_equ__gte='02001', cod_equ__lte='04090')
+    ),
         required=False,
         widget=forms.Select(attrs={'class': 'select2'})
     )
