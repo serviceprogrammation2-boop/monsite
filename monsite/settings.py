d2a0a507b2f93bcfde355c740e2d6454912f9bef
hedi <bmedh@hotmail.com>
2026-02-26 11:39:06 +0100

    Ajout WhiteNoise et compression static pour admin sur Render

--- monsite/settings.py
+++ monsite/settings.py
@@ -68,6 +68,7 @@ else:
 # -----------------------------
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -105,6 +106,8 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
+# Compression et cache pour WhiteNoise
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # -----------------------------
 # 🗄 TA BASE ORACLE (SI UTILISÉE)
 # -----------------------------
